aa3402b54f28bd4c472fbc4ba3f3f2287653d496
lsj0822 <lsj0822@khu.ac.kr>
2024-11-08 22:27:44 +0900

    feat(InputView) : 파일 읽기 기능 추가
    InputView에 readFile 메서드를 추가하였습니다.
    - 이는 FileReader객체와 BuffeeredReader 객체로 구현하였습니다.
    - 줄의 마지막까지 도달했을 때 while문을 종료하도록 구성하였습니다.
    - 리스트의 추가는 각 줄마다 별도의 메서드를 호출하여 생성하도록 구현하였습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,34 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
+
+    public static List<String> readFile(String fileAddress) {
+        try{
+            FileReader reader = new FileReader(fileAddress);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            return readFileToGroup(bufferedReader);
+        }catch(IOException exception){
+            OutputView.printErrorMessage(exception);
+        }
+        return null;
+    }
+
+    private static List<String> readFileToGroup(BufferedReader reader) throws IOException {
+        List<String> returnedList = new ArrayList<>();
+        String line;
+        while((line = reader.readLine()) != null){
+            returnedList.add(line);
+        }
+        return returnedList;
+    }
+
     public String inputItemsQuery(){
         OutputView.printMessage(Messages.INPUT_ITEMS_QUERY);
         return Console.readLine();
@@ -18,13 +44,8 @@ public class InputView {
         return Console.readLine();
     }
 
-    public String inputAskMembership(){
-        OutputView.printMessage(Messages.INPUT_ASK_MEMBERSHIP);
-        return Console.readLine();
-    }
-
-    public String inputAskBuyMore(){
-        OutputView.printMessage(Messages.INPUT_ASK_BYE_MORE);
+    public String inputAskCommon(Messages message){
+        OutputView.printMessage(message);
         return Console.readLine();
     }
 }
