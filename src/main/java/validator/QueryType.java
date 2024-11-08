b2230fd7ca9bc626e1c07d42baeded605ee044ef
lsj0822 <lsj0822@khu.ac.kr>
2024-11-08 22:36:15 +0900

    feat(QueryType) : QueryType enum 추가
    입력받은 각각의 String의 유효성을 확인하기 위해 Validator에 생성한 enum 클래스입니다.
    - enum 형식에 따라서 Validator에서 자연스럽게 유효성을 검사할 수 있도록 필드를 구성하였습니다.
    - queryLength는 Parse를 통해 나눠진 파일의 원소 수가 일치하는 지를 확인합니다
    - queryTypes는 각 원소의 인덱스에 맞게 그 원소가 맞는 타입인지를 확인할 수 있습니다.
    - queryType에 지정된 타입 외의 요소가 들어갈 수 없게 이 역시 Type이라는 enum 클래스로 구성하였습니다.

---
+++ src/main/java/validator/QueryType.java
@@ -0,0 +1,24 @@
+package validator;
+
+import java.util.List;
+
+public enum QueryType {
+    PRODUCT(4, List.of(Type.STRING, Type.INTEGER, Type.INTEGER, Type.STRING_OR_NULL)),
+    PROMOTION(5, List.of(Type.STRING, Type.INTEGER, Type.INTEGER, Type.LOCAL_DATE, Type.LOCAL_DATE));
+
+    private int queryLength;
+    private List<Type> queryTypes;
+
+    QueryType(int queryLength, List<Type> queryTypes) {
+        this.queryLength = queryLength;
+        this.queryTypes = queryTypes;
+    }
+
+    public int getQueryLength() {
+        return queryLength;
+    }
+
+    public List<Type> getQueryTypes() {
+        return queryTypes;
+    }
+}
---
+++ src/main/java/validator/Type.java
@@ -0,0 +1,8 @@
+package validator;
+
+public enum Type {
+    STRING,
+    INTEGER,
+    LOCAL_DATE,
+    STRING_OR_NULL;
+}
