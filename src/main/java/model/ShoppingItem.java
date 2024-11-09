19da5390e784bba314ba5c6342f0585b3f18b37f
lsj0822 <lsj0822@khu.ac.kr>
2024-11-09 20:10:44 +0900

    feat(ShoppingItem) : 인터페이스 추상 메서드 추가
    추상메서드를 추가하였습니다.
    - 해당 메서드들은 다형성을 위해 쇼핑리스트에서 사용할 메서드와 동일하게 구성이 되어있습니다.

--- src/main/java/model/ShoppingItem.java
+++ src/main/java/model/ShoppingItem.java
@@ -1,5 +1,19 @@
 package model;
 
 public interface ShoppingItem {
+    public abstract String getItemName();
 
+    public abstract Integer getPrice();
+
+    public abstract Promotion getPromotion();
+
+    public abstract Integer getQuantity();
+
+    public abstract boolean equals(ShoppingItem item);
+
+    public abstract void addNormalQuantity(Integer normalQuantity);
+
+    public abstract void addPromotionQuantity(Integer promotionQuantity);
+
+    public abstract String printItemStatus();
 }
