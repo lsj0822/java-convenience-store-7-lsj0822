b7c9547d3c92e388c5f1356e6d6f3f8f8b561cea
lsj0822 <lsj0822@khu.ac.kr>
2024-11-09 20:12:30 +0900

    feat(ShoppingList) : 쇼핑 리스트
    구매가 가능한 물품을 가지고 있는 쇼핑 리스트를 만들었습니다.
    - 해당 모델 클래스를 통해 리스트를 관리합니다.
    - 현재 products.md를 통해 받은 리스트의 값을 검증하고 해당 값읗 토대로 쇼핑리스트를 만들고 출력하는 기능을 가지고 있습니다.
    - 추가로 사용자가 구매를 완료했다면 적절한 Item을 찾아 값을 처리하는 로직이 필요합니다.

---
+++ src/main/java/model/ShoppingList.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import utils.Parser;
+import validator.QueryType;
+import validator.Validation;
+
+public class ShoppingList {
+    private ArrayList<ShoppingItem> shoppingItems = new ArrayList<>();
+
+    public ShoppingList(List<String> queries) {
+        initiateShoppingList(queries);
+    }
+
+    private void initiateShoppingList(List<String> queries) {
+        for (String query : queries) {
+            List<String> itemAttributes = Parser.parse(query);
+            Validation.ValidateContentLength(itemAttributes, QueryType.PRODUCT);
+            Validation.ValidateContentsType(itemAttributes, QueryType.PRODUCT);
+            ShoppingItem item = ItemFactory.generate(itemAttributes);
+            addShoppingList(item);
+        }
+    }
+
+    public void addShoppingList(ShoppingItem item) {
+        if (!isInShoppingList(item)) {
+            shoppingItems.add(item);
+        }
+    }
+
+    public boolean isInShoppingList(ShoppingItem addItem) {
+        boolean addResult = false;
+        Iterator<ShoppingItem> iterator = shoppingItems.iterator();
+        while (iterator.hasNext() && !addResult) {
+            ShoppingItem currentItem = iterator.next();
+            addResult = checkSingleShoppingItem(currentItem, addItem);
+        }
+        return addResult;
+    }
+
+    public boolean checkSingleShoppingItem(ShoppingItem shoppingItem, ShoppingItem addItem) {
+        boolean equalName = shoppingItem.getItemName().equals(addItem.getItemName());
+        boolean equalItem = shoppingItem.equals(addItem);
+        if (equalName && equalItem) {
+            addItemToExisted(shoppingItem, addItem);
+            return true;
+        }
+        if (equalName && !equalItem) {
+            throw new IllegalArgumentException("[Error] 이미 존재하는 물품입니다.");
+        }
+        return false;
+    }
+
+    public void addItemToExisted(ShoppingItem shoppingItem, ShoppingItem addItem) {
+        Integer quantity = addItem.getQuantity();
+        if (addItem instanceof NormalItem) {
+            shoppingItem.addNormalQuantity(quantity);
+        }
+        if (addItem instanceof PromotionItem) {
+            shoppingItem.addPromotionQuantity(quantity);
+        }
+    }
+
+    public ArrayList<String> printShoppingItems() {
+        ArrayList<String> printedItems = new ArrayList<>();
+        for (ShoppingItem shoppingItem : shoppingItems) {
+            printedItems.add(shoppingItem.printItemStatus());
+        }
+        return printedItems;
+    }
+
+}
