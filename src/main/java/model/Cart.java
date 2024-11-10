e62366d3cf4756bd3bf58088a2c92d8174ca5874
lsj0822 <lsj0822@khu.ac.kr>
2024-11-11 00:17:37 +0900

    feat(Cart) : 구매한 객체의 계산 로직 추가
    - 영수증에 출력할 문자열 포맷팅을 추가하였습니다.
    - BuyList에 반환할 결과를 게산하는 로직을 추가하였습니다.

--- src/main/java/model/Cart.java
+++ src/main/java/model/Cart.java
@@ -1,8 +1,11 @@
 package model;
 
 import java.util.List;
+import model.shoppingitem.NormalItem;
+import model.shoppingitem.PromotionItem;
 import model.shoppingitem.ShoppingItem;
 import store.BuyController;
+import utils.KoreanFormatter;
 import validator.Validation;
 
 public class Cart {
@@ -10,14 +13,15 @@ public class Cart {
     private Integer buyQuantity;
     private Integer buyPromotion;
     private Integer buyNormal;
+    private ShoppingItem matchedShoppingItem;
 
     public Cart(List<String> cartAttributes) {
         this.itemName = cartAttributes.get(0);
         this.buyQuantity = Integer.parseInt(cartAttributes.get(1));
         Validation.ValidateInShoppingList(itemName);
-        ShoppingItem findedShoppingItem = ShoppingList.findShoppingItem(itemName);
-        Validation.ValidateBuyQuantity(findedShoppingItem, buyQuantity);
-        BuyController.execute(findedShoppingItem, this);
+        matchedShoppingItem = ShoppingList.findShoppingItem(itemName);
+        Validation.ValidateBuyQuantity(matchedShoppingItem, buyQuantity);
+        BuyController.execute(matchedShoppingItem, this);
     }
 
     public Integer getBuyQuantity() {
@@ -28,4 +32,67 @@ public class Cart {
         this.buyPromotion = promotion;
         this.buyNormal = normal;
     }
+
+    public String toPrintTotal() {
+        return printItemNames() + String.format("%d\t%,d", toTotalQuantity(), toTotalPrice());
+    }
+
+    public String toPrintPromotion() {
+        if (matchedShoppingItem instanceof NormalItem) {
+            return null;
+        }
+        int promotionAppliedQuantity = buyPromotion / (matchedShoppingItem.getPromotion().getBuy() + 1);
+        if (promotionAppliedQuantity == 0) {
+            return null;
+        }
+        return printItemNames() + String.format("%d", promotionAppliedQuantity);
+    }
+
+    public String printItemNames() {
+        int spaces = KoreanFormatter.countSpace(itemName);
+        if (spaces <= 9) {
+            return itemName + "\t\t";
+        }
+        if (spaces <= 17) {
+            return itemName + "\t";
+        }
+        return KoreanFormatter.cutTooMuchLetters(itemName) + "\t";
+    }
+
+    public Integer toTotalQuantity() {
+        if (buyNormal == null && buyPromotion == null) {
+            return 0;
+        }
+        if (buyNormal == null) {
+            return buyPromotion;
+        }
+        if (buyPromotion == null) {
+            return buyNormal;
+        }
+        return buyNormal + buyPromotion;
+    }
+
+    public Integer toTotalPrice() {
+        return toTotalQuantity() * matchedShoppingItem.getPrice();
+    }
+
+    public Integer toTotalPromotedPrice() {
+        if (matchedShoppingItem instanceof NormalItem) {
+            return 0;
+        }
+        int promotionAppliedQuantity = buyPromotion / (matchedShoppingItem.getPromotion().getBuy() + 1);
+        return promotionAppliedQuantity * matchedShoppingItem.getPrice();
+    }
+
+    public Integer toNormalPrice() {
+        return buyNormal * matchedShoppingItem.getPrice();
+    }
+
+    public void substractFromShoppingItem() {
+        matchedShoppingItem.modifyNormalQuantity(-buyNormal);
+        if (matchedShoppingItem instanceof PromotionItem) {
+            matchedShoppingItem.modifyPromotionQuantity(-buyPromotion);
+        }
+    }
+
 }
