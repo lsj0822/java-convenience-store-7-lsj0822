2ce9f0a175662a1dd0fedade8ecfd732c23cba20
lsj0822 <lsj0822@khu.ac.kr>
2024-11-11 00:16:49 +0900

    feat(OutputView) : 영수증 출력 메서드 추가
    영수증 출력에 관한 메서드를 추가하였습니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.util.ArrayList;
+import model.BuyList;
 
 public class OutputView {
 
@@ -34,4 +35,48 @@ public class OutputView {
         }
     }
 
+    public static void printReceipt(BuyList buyList) {
+        printBuyTotalItems(buyList);
+        printPromotion(buyList);
+        printPriceInformation(buyList);
+    }
+
+    public static void printBuyTotalItems(BuyList buyList) {
+        printReceiptHeader();
+        ArrayList<String> prints = buyList.toPrintTotals();
+        for (String print : prints) {
+            System.out.println(print);
+        }
+    }
+
+    private static void printReceiptHeader() {
+        printBlank();
+        System.out.println(Messages.RECEIPT_STORE_HEADER.getMessage());
+        System.out.println(Messages.RECEIPT_COLUMN_HEADER.getMessage());
+    }
+
+    public static void printPromotion(BuyList buyList) {
+        System.out.println(Messages.RECEIPT_PROMOTION_HEADER.getMessage());
+        printBuyTotalPromotions(buyList);
+    }
+
+    public static void printBuyTotalPromotions(BuyList buyList) {
+        ArrayList<String> prints = buyList.toPrintPromotions();
+        for (String print : prints) {
+            System.out.println(print);
+        }
+    }
+
+    public static void printPriceInformation(BuyList buyList) {
+        System.out.println(Messages.RECEIPT_LINE.getMessage());
+        Integer totalQuantity = buyList.toTotalQuantity();
+        Integer initialPrice = buyList.toTotalPrice();
+        Integer promotedPrice = buyList.toPromotedPrice();
+        Integer membershipDiscountPrice = buyList.toMemberShipPrice();
+        Integer finalPrice = initialPrice - promotedPrice - membershipDiscountPrice;
+        System.out.printf("총구매액\t\t%,d\t%,8d\n", totalQuantity, initialPrice);
+        System.out.printf("행사할인\t\t\t%,8d\n", -promotedPrice);
+        System.out.printf("멤버십할인\t\t\t%,8d\n", -membershipDiscountPrice);
+        System.out.printf("내실돈\t\t\t%,8d\n", finalPrice);
+    }
 }
