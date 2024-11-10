90808d20be2005912450b1853f4c1da2f0677c5b
lsj0822 <lsj0822@khu.ac.kr>
2024-11-11 00:18:19 +0900

    feat(BuyList) : 결과 반환 로직 추가
    - 결과를 출력하기 위한 로직을 추가하였습니다.
    - 이는 각각의 객체에 대해 결과를 Cart로 부터 계산하여 이를 더하는 방식으로 구현하였습니다.

--- src/main/java/model/BuyList.java
+++ src/main/java/model/BuyList.java
@@ -23,8 +23,63 @@ public class BuyList {
         }
     }
 
-    public Integer getCartSize() {
-        return carts.size();
+    public ArrayList<String> toPrintTotals() {
+        ArrayList<String> returnContents = new ArrayList<>();
+        for (Cart cart : carts) {
+            returnContents.add(cart.toPrintTotal());
+        }
+        return returnContents;
     }
 
+    public ArrayList<String> toPrintPromotions() {
+        ArrayList<String> returnedContents = new ArrayList<>();
+        for (Cart cart : carts) {
+            if (cart.toPrintPromotion() == null) {
+                continue;
+            }
+            returnedContents.add(cart.toPrintPromotion());
+        }
+        return returnedContents;
+    }
+
+    public Integer toTotalQuantity() {
+        Integer totalQuantity = 0;
+        for (Cart cart : carts) {
+            totalQuantity += cart.toTotalQuantity();
+        }
+        return totalQuantity;
+    }
+
+    public Integer toTotalPrice() {
+        Integer totalPrice = 0;
+        for (Cart cart : carts) {
+            totalPrice += cart.toTotalPrice();
+        }
+        return totalPrice;
+    }
+
+    public Integer toPromotedPrice() {
+        Integer totalPromotedPrice = 0;
+        for (Cart cart : carts) {
+            totalPromotedPrice += cart.toTotalPromotedPrice();
+        }
+        return totalPromotedPrice;
+    }
+
+    public Integer toMemberShipPrice() {
+        Integer totalNormalPrice = 0;
+        if (!membership) {
+            return 0;
+        }
+        for (Cart cart : carts) {
+            totalNormalPrice += cart.toNormalPrice();
+        }
+        return totalNormalPrice * 3 / 10;
+    }
+
+    public void substractQuantity() {
+        for (Cart cart : carts) {
+            cart.substractFromShoppingItem();
+        }
+    }
 }
