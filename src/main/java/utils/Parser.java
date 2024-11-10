60c83eb8d14e9ed5146f338e936315308b89a777
lsj0822 <lsj0822@khu.ac.kr>
2024-11-10 17:49:38 +0900

    feat(Parser) : 기능 추가
    이 프로그램에서 Parsing은 2가지 경우에 발생합니다.
    - 쉼표를 기준으로 파싱(resource파일 및 사용자가 입력한 쿼리)
    - 대시(-)를 기준으로 파싱(쿼리의 입력 방식)
    그에 맞게 상황에 따른 파싱이 가능하도록 수정하였습니다.

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -1,17 +1,36 @@
 package utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
-    public final static String DELIMITER = ",";
+    public final static String CONTENT_DELIMITER = ",";
+    public final static String QUERY_DELIMITER = "-";
+    public final static String BRACKETS_LEFT = "[";
+    public final static String BRACKETS_RIGHT = "]";
 
     public static List<String> parse(String inputContent) {
-        List<String> parsedContent = splitting(inputContent);
+        List<String> parsedContent = splitting(inputContent, CONTENT_DELIMITER);
         return trim(parsedContent);
     }
 
-    private static List<String> splitting(String inputContent) {
-        return List.of(inputContent.split(DELIMITER));
+    public static ArrayList<List<String>> parseQuery(List<String> inputQueries) {
+        ArrayList<List<String>> returnContent = new ArrayList<>();
+        for (String inputQuery : inputQueries) {
+            returnContent.add(parseSingleQuery(inputQuery));
+        }
+        return returnContent;
+    }
+
+    public static List<String> parseSingleQuery(String query) {
+        int left = query.indexOf(BRACKETS_LEFT), right = query.indexOf(BRACKETS_RIGHT);
+        String extractedQuery = query.substring(left + 1, right);
+        List<String> parsedQuery = splitting(extractedQuery, QUERY_DELIMITER);
+        return trim(parsedQuery);
+    }
+
+    private static List<String> splitting(String inputContent, String delimiter) {
+        return List.of(inputContent.split(delimiter));
     }
 
     private static List<String> trim(List<String> splittedContent) {
