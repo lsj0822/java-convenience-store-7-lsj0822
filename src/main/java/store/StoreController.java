717e4bcf699d92d817d693a8ced5e5897b07418a
lsj0822 <lsj0822@khu.ac.kr>
2024-11-10 17:55:41 +0900

    feat(StoreController) : StoreController 추가
    전반적인 프로그램을 관리하는 클래스입니다.
    - 초기의 재고 관리는 MarketInformationController를 통해 이루어집니다.
    - 이후 사용자의 입력은 programController에 의해 이루어집니다.

--- src/main/java/store/StoreController.java
+++ src/main/java/store/StoreController.java
@@ -1,6 +1,13 @@
 package store;
 
+import java.util.ArrayList;
 import java.util.List;
+import model.BuyList;
+import model.PromotionList;
+import model.ShoppingList;
+import utils.Parser;
+import validator.Validation;
+import view.ErrorMessages;
 import view.InputView;
 import view.Messages;
 import view.OutputView;
@@ -10,12 +17,59 @@ public class StoreController {
     private final String promotionsFileAddress = "src/main/resources/promotions.md";
 
     public void start() {
-        CVSInformationController();
+        MarketInformationController();
+        programController();
     }
 
-    public void CVSInformationController() {
-        OutputView.printMessage(Messages.OUTPUT_WELCOME_MESSAGE);
+    public void MarketInformationController() {
         List<String> products = InputView.readFile(productsFileAddress);
         List<String> promotions = InputView.readFile(promotionsFileAddress);
+        PromotionList promotionList = new PromotionList(promotions);
+        ShoppingList shoppingList = new ShoppingList(products);
+    }
+
+    public void programController() {
+        OutputView.printMessage(Messages.OUTPUT_WELCOME_MESSAGE);
+        OutputView.printGroups(ShoppingList.printShoppingItems());
+        InputItemController();
+    }
+
+    public void InputItemController() {
+        try {
+            String inputItemsQuery = InputView.inputItemsQuery();
+            List<String> parsedQuery = Parser.parse(inputItemsQuery);
+            Validation.ValidateItemsQuery(parsedQuery);
+            ArrayList<List<String>> itemQuery = Parser.parseQuery(parsedQuery);
+            ShoppingController(itemQuery);
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception);
+            InputItemController();
+        }
+    }
+
+    public void ShoppingController(ArrayList<List<String>> inputQuery) {
+        try {
+            BuyList buyList = new BuyList(inputQuery);
+            String query = InputView.inputAskCommon(Messages.INPUT_ASK_MEMBERSHIP).toUpperCase();
+            buyList.setMembership(membershipSetting(query));
+            printReceipt(buyList);
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception);
+        }
+        //ShoppingController(inputQuery);
+    }
+
+    public boolean membershipSetting(String query) {
+        if (query.startsWith("Y")) {
+            return true;
+        }
+        if (query.startsWith("N")) {
+            return false;
+        }
+        throw new IllegalArgumentException(ErrorMessages.ERROR_ETC.getMessage());
+    }
+
+    public void printReceipt(BuyList buyList) {
+        System.out.println(buyList.getCartSize());
     }
 }
